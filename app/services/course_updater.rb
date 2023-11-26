35e9f72b37b700bb7a6499775abf905bf9d92e8a
Weilong <fan01856472@gmail.com>
2023-11-26 23:55:14 +0800

    refactor CourseUpdater

--- app/services/course_updater.rb
+++ app/services/course_updater.rb
@@ -1,55 +1,73 @@
 class CourseUpdater
- 
+
+  attr_accessor :course, :course_params
+  
   def initialize(id, params)
-    @course_id = id
+    @course = Course.preload(chapters: :units).find(id)
     @course_params = params
-    @course_mapper = CourseMapper.new
   end
 
   def execute
-    course = Course.preload(chapters: :units).find(@course_id)
-    course.name = @course_params[:name]
-    course.lecturer = @course_params[:lecturer]
-    course.description = @course_params[:description]
-
-    @course_mapper.set_course(course)
-
-    @course_params[:chapters].each do |chapter_params|
-      update_chapter(course, chapter_params)
+    ActiveRecord::Base.transaction do 
+      map_course_info
+      course.save!
     end
-
-    @course_mapper.commit
   end
 
   private
 
-  def update_chapter(course, chapter_params)
-    chapter = course&.chapters&.find { |chapter| chapter.id == chapter_params[:id] } 
-
-    @course_mapper.set_chapter(
-      {
-        name: chapter_params[:name],
-        sort_key: chapter_params[:sort_key]
-      }, 
-      chapter&.id
+  def map_course_info
+    course.assign_attributes(
+      name: course_params[:name],
+      lecturer: course_params[:lecturer],
+      description: course_params[:description]
     )
+    
+    chapter_index = 0
 
-    chapter_params[:units].each do |unit_params|
-      update_unit(chapter, unit_params)
+    course_params[:chapters].each do |chapter_params|
+      chapter = course&.chapters&.find { |c| c.id == chapter_params[:id] } 
+      if chapter.nil?
+        chapter = course.chapters.create(
+          name: chapter_params[:name],
+          sort_key: chapter_index
+        )
+      elsif chapter && chapter_params[:_deleted].present?
+        chapter.destroy!
+        next
+      else
+        Chapter.where(id: chapter_params[:id]).update_all(
+          name: chapter_params[:name],
+          sort_key: chapter_index
+        )
+      end
+      
+      unit_index = 0
+      chapter_params[:units].each do |unit_params|
+        unit = chapter&.units.find { |u| u.id == unit_params[:id] } 
+        if unit.nil?
+          chapter.units.create(
+            name: unit_params[:name],
+            description: unit_params[:description],
+            content: unit_params[:content],
+            sort_key: unit_index
+          )
+        elsif unit && unit_params[:_deleted].present?
+          unit.destroy!
+          next
+        else
+          Unit.where(id: unit_params[:id]).update_all(
+            name: unit_params[:name],
+            description: unit_params[:description],
+            content: unit_params[:content],
+            sort_key: unit_index
+          )
+        end
+
+        unit_index += 1
+      end
+
+      chapter_index += 1
     end
-  end
-
-  def update_unit(chapter, unit_params)
-    unit = chapter&.units&.find { |unit| unit.id == unit_params[:id] } 
-
-    @course_mapper.set_unit(
-      {
-        name: unit_params[:name],
-        description: unit_params[:description],
-        content: unit_params[:content],
-        sort_key: unit_params[:sort_key]
-      },  
-      unit&.id
-    )
   end
 end
--- spec/services/course_updater_spec.rb
+++ spec/services/course_updater_spec.rb
@@ -1,93 +1,199 @@
 require "rails_helper"
 
 RSpec.describe CourseUpdater do
+  let!(:course) { create(:course, name: "JavaScript")}
+  let!(:chapter1) { create(:chapter, course: course, name: "Draft")}
+  let!(:unit1) { create(:unit, chapter: chapter1, name: "Draft")}
+
+  let(:course_params) {
+    {
+      name: "Ruby on Rails",
+      lecturer: "test",
+      description: "test",
+      chapters: chapters
+    }
+  }
 
   describe "#execute" do
-    let!(:course) { create(:course, name: "JavaScript")}
-    let!(:chapter1) { create(:chapter, course: course, name: "Draft")}
-    let!(:unit1) { create(:unit, chapter: chapter1, name: "Draft")}
-    
-    context "when update existed chapters and units" do 
-      let(:course_params) {
-        {
-          name: "Ruby on Rails",
-          lecturer: "test",
-          description: "test",
-          chapters: [
-            {
-              id: chapter1.id,
-              name: "Chapter A",
-              sort_key: 0,
-              units: [
-                {
-                  id: unit1.id,
-                  name: "Unit A-1",
-                  description: "",
-                  content: "Hello World",
-                  sort_key: 0
-                }
-              ]
-            }
-          ]
-        }
+    context "when update" do 
+      let(:chapters) {
+        [
+          {
+            id: chapter1.id,
+            name: "Chapter A",
+            units: [
+              { 
+                id: unit1.id,
+                name: "Unit A-1",
+                description: "",
+                content: "Hello World",
+              }
+            ]
+          }
+        ]
       }
+
       it "return true" do
         expect(CourseUpdater.new(course.id, course_params).execute).to eq(true)
 
         course.reload
-        expect(course.name).to eq("Ruby on Rails")
-        expect(course.chapters.length).to eq(1)
+        expect(course).to have_attributes(
+          name: "Ruby on Rails",
+          lecturer: "test",
+          description: "test"
+        )
 
         chapter1.reload
-        expect(chapter1.name).to eq("Chapter A")
+        expect(chapter1).to have_attributes(
+          name: "Chapter A",
+          sort_key: 0
+        )
 
         unit1.reload
-        expect(unit1.name).to eq("Unit A-1")
+        expect(unit1).to have_attributes(
+          name: "Unit A-1",
+          description: "",
+          content: "Hello World",
+          sort_key: 0
+        )
+      end
+
+      it "the number of chapters and units remain" do
+        expect{ CourseUpdater.new(course.id, course_params).execute }.to \
+          change(Chapter, :count).by(0).and \
+          change(Unit, :count).by(0)
       end
     end
 
-    context "when update non-existed chapters and units" do 
-      let(:course_params) {
-        {
-          name: "Ruby on Rails",
-          lecturer: "test",
-          description: "test",
-          chapters: [
-            {
-              id: nil,
-              name: "Chapter B",
-              sort_key: 1,
-              units: [
-                {
-                  name: "Unit B-1",
-                  description: "",
-                  content: "Hello World",
-                  sort_key: 0
-                },
-                {
-                  name: "Unit B-2",
-                  description: "",
-                  content: "Hello World",
-                  sort_key: 1
-                }
-              ]
-            }
-          ]
-        }
+    context "when update with a new unit" do
+      let(:chapters) {
+        [
+          {
+            id: chapter1.id,
+            name: "Chapter A",
+            units: [
+              { 
+                id: unit1.id,
+                name: "Unit A-1",
+                description: "",
+                content: "Hello World",
+              },
+              { # add a new unit to the chapter
+                name: "Unit A-2",
+                description: "",
+                content: "Hello World",
+              }
+            ]
+          }
+        ]
       }
+
       it "return true" do
         expect(CourseUpdater.new(course.id, course_params).execute).to eq(true)
+      end
 
-        course.reload
-        expect(course.name).to eq("Ruby on Rails")
-        expect(course.chapters.length).to eq(2)
+      it "increase units by 1" do
+        expect{ CourseUpdater.new(course.id, course_params).execute }.to \
+          change(Chapter, :count).by(0).and \
+          change(Unit, :count).from(1).to(2)
+      end
+    end
 
-        chapter2 = course.chapters.last
-        expect(chapter2.name).to eq("Chapter B")
-        expect(chapter2.units.length).to eq(2)
-        
-        unit2 = chapter2.units.first
-        expect(unit2.name).to eq("Unit B-1")
+    context "when update with a new chapter and unit" do
+      let(:chapters) {
+        [
+          {
+            id: chapter1.id,
+            name: "Chapter A",
+            units: [
+              { 
+                id: unit1.id,
+                name: "Unit A-1",
+                description: "",
+                content: "Hello World",
+              }
+            ]
+          },
+          { # add a new chapter and unit to the course
+            name: "Chapter B",
+            units: [
+              { 
+                name: "Unit B-1",
+                description: "",
+                content: "Hello World",
+              }
+            ]
+          }
+        ]
+      }
+
+      it "returns true" do
+        expect(CourseUpdater.new(course.id, course_params).execute).to eq(true)
+      end
+
+      it "increases the number of chapters and units" do
+        expect{ CourseUpdater.new(course.id, course_params).execute }.to \
+          change(Chapter, :count).from(1).to(2).and \
+          change(Unit, :count).from(1).to(2)
+      end
+    end
+
+    context "when update and delete a chapter" do
+      let(:chapters) {
+        [
+          {
+            id: chapter1.id,
+            name: "Chapter A",
+            _deleted: true, # delete the chapter 
+            units: [
+              { 
+                id: unit1.id,
+                name: "Unit A-1",
+                description: "",
+                content: "Hello World"
+              }
+            ]
+          }
+        ]
+      }
+
+      it "returns true" do
+        expect(CourseUpdater.new(course.id, course_params).execute).to eq(true)
+      end
+
+      it "deletes the chapter and relevant units" do
+        expect{ CourseUpdater.new(course.id, course_params).execute }.to \
+          change(Chapter, :count).from(1).to(0).and \
+          change(Unit, :count).from(1).to(0)
+      end
+    end
+
+    context "when update and delete a unit" do
+      let(:chapters) {
+        [
+          {
+            id: chapter1.id,
+            name: "Chapter A",
+            units: [
+              {  # delete the unit
+                id: unit1.id,
+                name: "Unit A-1",
+                description: "",
+                content: "Hello World",
+                _deleted: true
+              }
+            ]
+          }
+        ]
+      }
+
+      it "return true" do
+        expect(CourseUpdater.new(course.id, course_params).execute).to eq(true)
+      end
+
+      it "deletes the unit" do
+        expect{ CourseUpdater.new(course.id, course_params).execute }.to \
+          change(Unit, :count).from(1).to(0)
       end
     end
   end
